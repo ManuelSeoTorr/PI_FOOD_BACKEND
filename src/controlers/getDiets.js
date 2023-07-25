f8515dd44a25b71813aa6092f2a311644774feca
ManuelSeoTorr <manst94@gmail.com>
2023-07-24 22:56:12 -0300

    deploy7

--- src/controlers/getDiets.js
+++ src/controlers/getDiets.js
@@ -1,48 +1,41 @@
 const axios = require("axios");
 const { API_KEY } = process.env;
-const { Diet } = require("../db");
+const { Diet } =require("../db");
 
 const getDiets = async () => {
-  try {
-    const allDiets = await Diet.findAll(); //traigo todas las diets en mi DB
-    if (!allDiets.length) await setDiets();
+    const allDiets = await Diet.findAll() //traigo todas las diets en mi DB 
     return allDiets;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 const setDiets = async () => {
-  // try {
-  //     const {data} = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`) //traigo 100 recetas de la API
-  //     const filteredDiets = data.results.map(({diets})=> diets )//armo un array con los arrays de las diets
-  //     const uniqueDiets = new Set (filteredDiets.flat())//new Set no permite duplicados, el flat une los arrays
-  //     const dietsObj =[...uniqueDiets].map((diet) => ({ name: diet }));//mapeo para agregarle la propiedad name
-  //     await Diet.bulkCreate(dietsObj);
-  //     return dietsObj;
-  // } catch (error) {
-  //     console.log(error.message);
-  // }
-  try {
-    const { data } = await axios(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`
-    ); //traigo 100 recetas de la API
-    const filteredDiets = data.results.map(({ diets }) => diets); //armo un array con los arrays de las diets
-    const uniqueDiets = new Set(filteredDiets.flat()); //new Set no permite duplicados, el flat une los arrays
-    uniqueDiets.forEach((diet) => {
-      Diet.findOrCreate({
-        where: { name: diet },
-      });
-    });
-    const dietsObj = await Diet.findAll();
-    console.log(dietsObj);
-    return dietsObj;
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+    // try {
+    //     const {data} = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`) //traigo 100 recetas de la API
+    //     const filteredDiets = data.results.map(({diets})=> diets )//armo un array con los arrays de las diets
+    //     const uniqueDiets = new Set (filteredDiets.flat())//new Set no permite duplicados, el flat une los arrays
+    //     const dietsObj =[...uniqueDiets].map((diet) => ({ name: diet }));//mapeo para agregarle la propiedad name
+    //     await Diet.bulkCreate(dietsObj);
+    //     return dietsObj;
+    // } catch (error) {
+    //     console.log(error.message);    
+    // }
+    try {
+        const {data} = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`) //traigo 100 recetas de la API
+        const filteredDiets = data.results.map(({diets})=> diets )//armo un array con los arrays de las diets
+        const uniqueDiets = new Set (filteredDiets.flat())//new Set no permite duplicados, el flat une los arrays
+        uniqueDiets.forEach(diet => {
+            Diet.findOrCreate ({
+                where: {name:diet}
+            })
+        });
+        const dietsObj = await Diet.findAll();
+        console.log(dietsObj);
+        return dietsObj;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 module.exports = {
-  getDiets,
-  setDiets,
-};
+    getDiets,
+    setDiets
+}
