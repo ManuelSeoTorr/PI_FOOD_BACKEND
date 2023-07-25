878f9e75aa74ab4370f5cb8573feaa409603e986
ManuelSeoTorr <manst94@gmail.com>
2023-07-24 23:13:43 -0300

    deploy8

--- src/controlers/getDiets.js
+++ src/controlers/getDiets.js
@@ -19,6 +19,8 @@ const setDiets = async () => {
     //     console.log(error.message);    
     // }
     try {
+        const allDiets = await Diet.findAll()
+        if(allDiets.length) return;
         const {data} = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`) //traigo 100 recetas de la API
         const filteredDiets = data.results.map(({diets})=> diets )//armo un array con los arrays de las diets
         const uniqueDiets = new Set (filteredDiets.flat())//new Set no permite duplicados, el flat une los arrays
